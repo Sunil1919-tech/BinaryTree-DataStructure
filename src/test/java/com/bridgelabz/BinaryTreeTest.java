c7ccb7f21e259873e774c553f10988a4807e00a5
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-30 23:27:41 +0530

    Added: create binary tree with masx. size

--- src/test/java/com/bridgelabz/BinaryTreeTest.java
+++ src/test/java/com/bridgelabz/BinaryTreeTest.java
@@ -5,12 +5,22 @@ import org.junit.jupiter.api.Test;
 
 public class BinaryTreeTest {
     @Test
-    public void givenThreeNumbers_WhenAddedToBST_ShouldReturnSize() {
+    public void givenNumbers_WhenAddedToBST_ShouldReturnSize() {
         MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
         myBinaryTree.add(56);
         myBinaryTree.add(30);
         myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
         int size = myBinaryTree.getSize();
-        Assertions.assertEquals(3,size);
+        Assertions.assertEquals(13,size);
     }
 }
